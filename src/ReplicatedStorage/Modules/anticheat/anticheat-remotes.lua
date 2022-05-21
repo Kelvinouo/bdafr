d0c4ffc9ea2beadfbcb9c042bb446f7f36b9e3eb
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:42:39 +0800

    Update

---
+++ src/ReplicatedStorage/Modules/anticheat/anticheat-remotes.lua
@@ -0,0 +1,9 @@
+-- Decompiled with the Synapse X Luau decompiler.
+
+local v1 = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
+local v2 = v1.import(script, v1.getModule(script, "@rbxts", "net").out);
+return {
+	AnticheatRemotes = v2.Definitions.Create({
+		ReportStatus = v2.Definitions.ClientToServerEvent()
+	})
+};
