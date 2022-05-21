83ca920f11fa19f45580e68e4a3b7de1a1746a86
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:39:26 +0800

    Update

---
+++ src/Players/IIIlIlIllllllIIlllIl/PlayerScripts/TS/controllers/global/battle-pass/battle-pass-reward-display-type.lua
@@ -0,0 +1,17 @@
+-- Decompiled with the Synapse X Luau decompiler.
+
+local v1 = {};
+local v2 = setmetatable({}, {
+	__index = v1
+});
+v2.KIT = "kit";
+v1.kit = "KIT";
+v2.KIT_SKIN = "kitSkin";
+v1.kitSkin = "KIT_SKIN";
+v2.KILL_EFFECT = "killEffect";
+v1.killEffect = "KILL_EFFECT";
+v2.LOBBY_GADGET = "lobbyGadget";
+v1.lobbyGadget = "LOBBY_GADGET";
+return {
+	BPRewardDisplayType = v2
+};
