cb67b01ca4befed5e1be5f50e96df48372027c65
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:38:43 +0800

    Update

---
+++ src/Players/IIIlIlIllllllIIlllIl/PlayerScripts/TS/controllers/global/status-effect/ui/events/status-effect-activated-tag-event.lua
@@ -0,0 +1,19 @@
+-- Decompiled with the Synapse X Luau decompiler.
+
+local v1 = setmetatable({}, {
+	__tostring = function()
+		return "StatusEffectActivatedTagEvent";
+	end
+});
+v1.__index = v1;
+function v1.new(...)
+	local v2 = setmetatable({}, v1);
+	return v2:constructor(...) and v2;
+end;
+function v1.constructor(p1, p2, p3)
+	p1.statusEffect = p2;
+	p1.player = p3;
+end;
+return {
+	StatusEffectActivatedTagEvent = v1
+};
