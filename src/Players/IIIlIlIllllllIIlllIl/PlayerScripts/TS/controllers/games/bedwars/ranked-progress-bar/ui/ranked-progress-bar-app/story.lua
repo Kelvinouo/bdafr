5d1a3542d31894792be4bd8654da3cd4ed70222b
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:40:24 +0800

    Update

---
+++ src/Players/IIIlIlIllllllIIlllIl/PlayerScripts/TS/controllers/games/bedwars/ranked-progress-bar/ui/ranked-progress-bar-app/story.lua
@@ -0,0 +1,29 @@
+-- Decompiled with the Synapse X Luau decompiler.
+
+local v1 = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
+local l__RankDivision__1 = v1.import(script, game:GetService("ReplicatedStorage"), "TS", "rank", "rank-division").RankDivision;
+local l__CreateRoduxApp__2 = v1.import(script, script.Parent.Parent.Parent.Parent.Parent.Parent, "ui", "rodux", "create-rodux-app").CreateRoduxApp;
+local l__RankedProgressBarCore__3 = v1.import(script, script.Parent, "ranked-progress-bar-core").RankedProgressBarCore;
+local u4 = v1.import(script, v1.getModule(script, "@rbxts", "roact").src);
+return function(p1)
+	local u5 = l__CreateRoduxApp__2("RankedProgressBarApp", l__RankedProgressBarCore__3, {
+		RankChangeData = {
+			oldDivision = {
+				division = l__RankDivision__1.DIAMOND_3, 
+				rankPoints = 80
+			}, 
+			newDivision = {
+				division = l__RankDivision__1.DIAMOND_4, 
+				rankPoints = 10
+			}, 
+			rpDelta = 110, 
+			rankVisible = true, 
+			matchesPlayed = 10
+		}
+	}, {}, {
+		Parent = p1
+	});
+	return function()
+		u4.unmount(u5);
+	end;
+end;
