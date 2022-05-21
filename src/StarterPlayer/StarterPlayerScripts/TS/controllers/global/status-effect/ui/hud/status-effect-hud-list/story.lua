9e7a7b0a20b9d2a9f2ea1fa183231d9c70605c20
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:36:58 +0800

    Update

---
+++ src/StarterPlayer/StarterPlayerScripts/TS/controllers/global/status-effect/ui/hud/status-effect-hud-list/story.lua
@@ -0,0 +1,17 @@
+-- Decompiled with the Synapse X Luau decompiler.
+
+local v1 = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
+local u1 = v1.import(script, v1.getModule(script, "@rbxts", "roact").src);
+local u2 = v1.import(script, script.Parent, "status-effect-hud-list").StatusEffectHudListScreen;
+local l__StatusEffectType__3 = v1.import(script, game:GetService("ReplicatedStorage"), "TS", "status-effect", "status-effect-type").StatusEffectType;
+return function(p1)
+	local u4 = u1.mount(u1.createElement(u2, {
+		StatusEffects = { {
+				statusEffect = l__StatusEffectType__3.FORCEFIELD, 
+				expireTime = 90
+			} }
+	}), p1);
+	return function()
+		return u1.unmount(u4);
+	end;
+end;
