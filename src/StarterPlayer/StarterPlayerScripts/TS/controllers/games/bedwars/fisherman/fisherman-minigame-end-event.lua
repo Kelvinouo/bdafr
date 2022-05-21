7c81f439c4c7a0ec83a633fb6eb9520d8e8810f4
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:34:38 +0800

    Update

--- src/StarterPlayer/StarterPlayerScripts/TS/controllers/games/bedwars/fisherman/fisherman-minigame-end-event.lua
+++ src/StarterPlayer/StarterPlayerScripts/TS/controllers/games/bedwars/fisherman/fisherman-minigame-end-event.lua
@@ -1,1 +1,20 @@
-Hello 5
+-- Decompiled with the Synapse X Luau decompiler.
+
+local v1 = setmetatable({}, {
+	__tostring = function()
+		return "FishermanMinigameEndEvent";
+	end
+});
+v1.__index = v1;
+function v1.new(...)
+	local v2 = setmetatable({}, v1);
+	return v2:constructor(...) and v2;
+end;
+function v1.constructor(p1, p2, p3, p4)
+	p1.gameOver = p2;
+	p1.win = p3;
+	p1.timeElapsed = p4;
+end;
+return {
+	FishermanMinigameEndEvent = v1
+};
