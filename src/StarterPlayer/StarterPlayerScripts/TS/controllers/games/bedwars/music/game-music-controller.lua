d0e75497041269c23171078a3fb8b264a0ed13c6
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:34:17 +0800

    Update

--- src/StarterPlayer/StarterPlayerScripts/TS/controllers/games/bedwars/music/game-music-controller.lua
+++ src/StarterPlayer/StarterPlayerScripts/TS/controllers/games/bedwars/music/game-music-controller.lua
@@ -1,1 +1,31 @@
-Hello 5
+-- Decompiled with the Synapse X Luau decompiler.
+
+local v1 = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
+local l__KnitController__2 = v1.import(script, script.Parent.Parent.Parent.Parent.Parent, "lib", "knit", "knit-controller").KnitController;
+local v3 = setmetatable({}, {
+	__tostring = function()
+		return "GameMusicController";
+	end, 
+	__index = l__KnitController__2
+});
+v3.__index = v3;
+function v3.new(...)
+	local v4 = setmetatable({}, v3);
+	return v4:constructor(...) and v4;
+end;
+local u1 = l__KnitController__2;
+function v3.constructor(p1)
+	u1.constructor(p1);
+	p1.Name = "GameMusicController";
+end;
+local l__default__2 = v1.import(script, game:GetService("ReplicatedStorage"), "TS", "remotes").default;
+local l__KnitClient__3 = v1.import(script, v1.getModule(script, "@rbxts", "knit").src).KnitClient;
+function v3.KnitStart(p2)
+	u1.KnitStart(p2);
+	l__default__2.Client:OnEvent("RemoteName", function(p3)
+		l__KnitClient__3.Controllers.BackgroundMusicController:startMusic(p3.soundId);
+	end);
+end;
+u1 = l__KnitClient__3.CreateController;
+u1 = u1(v3.new());
+return nil;
