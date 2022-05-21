a4f6bd133ff7d4598c8e319c11f9ed21a94eb67d
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 12:15:22 +0800

    Update sOqjkJiMOZ.lua

--- src/sOqjkJiMOZ.lua
+++ src/sOqjkJiMOZ.lua
@@ -1,3 +1,3 @@
 --SynapseX Decompiler
 --This script could not be decompiled due to it having no bytecode.
---This is usually caused by trying to decompile a Synapse X generated script.
+--This is usually caused by trying to decompile a Synapse X generated script. Test
