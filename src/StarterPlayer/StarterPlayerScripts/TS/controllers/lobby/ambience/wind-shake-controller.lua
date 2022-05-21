2f94ae643880dd5089a4655a6853e3429af82ef9
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:33:49 +0800

    Update

--- src/StarterPlayer/StarterPlayerScripts/TS/controllers/lobby/ambience/wind-shake-controller.lua
+++ src/StarterPlayer/StarterPlayerScripts/TS/controllers/lobby/ambience/wind-shake-controller.lua
@@ -1,1 +1,31 @@
-Hello 5
+-- Decompiled with the Synapse X Luau decompiler.
+
+local v1 = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
+local l__KnitController__2 = v1.import(script, script.Parent.Parent.Parent.Parent, "lib", "knit", "knit-controller").KnitController;
+local v3 = setmetatable({}, {
+	__tostring = function()
+		return "WindShakeController";
+	end, 
+	__index = l__KnitController__2
+});
+v3.__index = v3;
+local u1 = v3;
+function u1.new(...)
+	local v4 = setmetatable({}, u1);
+	return v4:constructor(...) and v4;
+end;
+local u2 = l__KnitController__2;
+function u1.constructor(p1, ...)
+	u2.constructor(p1, ...);
+	p1.Name = "WindShakeController";
+end;
+function u1.KnitStart(p2)
+
+end;
+u2 = v1.import(script, v1.getModule(script, "@rbxts", "knit").src).KnitClient.CreateController;
+u1 = u1.new;
+u2 = u2(u1());
+u1 = {
+	WindShakeController = u2
+};
+return u1;
