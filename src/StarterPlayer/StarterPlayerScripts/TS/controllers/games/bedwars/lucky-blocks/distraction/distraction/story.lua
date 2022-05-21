dce7bcba582684ac2fbd9ac25697d6d63b9573b9
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:35:11 +0800

    Update

--- src/StarterPlayer/StarterPlayerScripts/TS/controllers/games/bedwars/lucky-blocks/distraction/distraction/story.lua
+++ src/StarterPlayer/StarterPlayerScripts/TS/controllers/games/bedwars/lucky-blocks/distraction/distraction/story.lua
@@ -1,1 +1,11 @@
-Hello 5
+-- Decompiled with the Synapse X Luau decompiler.
+
+local v1 = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
+local u1 = v1.import(script, v1.getModule(script, "@rbxts", "roact").src);
+local l__Distraction__2 = v1.import(script, script.Parent, "distraction").Distraction;
+return function(p1)
+	local u3 = u1.mount(u1.createElement(l__Distraction__2), p1);
+	return function()
+		return u1.unmount(u3);
+	end;
+end;
