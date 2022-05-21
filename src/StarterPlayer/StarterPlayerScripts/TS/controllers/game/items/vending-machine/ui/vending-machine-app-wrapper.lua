b3ffbbbac91b473864395e1ccd019440a751081b
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:35:29 +0800

    Update

---
+++ src/StarterPlayer/StarterPlayerScripts/TS/controllers/game/items/vending-machine/ui/vending-machine-app-wrapper.lua
@@ -0,0 +1,35 @@
+-- Decompiled with the Synapse X Luau decompiler.
+
+local v1 = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
+local l__Maid__1 = v1.import(script, v1.getModule(script, "@rbxts", "knit").src).Maid;
+local u2 = v1.import(script, v1.getModule(script, "@rbxts", "roact").src);
+local l__VendingMachineApp__3 = v1.import(script, script.Parent, "vending-machine-app").VendingMachineApp;
+return {
+	VendingMachineAppWrapper = v1.import(script, v1.getModule(script, "@rbxts", "roact-hooks").src).new(u2)(function(p1, p2)
+		local l__useState__2 = p2.useState;
+		local v3, v4 = l__useState__2(p1.vendingMachine:GetAttribute("VendingMachineState"));
+		local v5, v6 = l__useState__2(p1.vendingMachine:GetAttribute("ChoosenReward"));
+		local v7, v8 = l__useState__2(p1.vendingMachine:GetAttribute("VendingMachineDispenseTime"));
+		p2.useEffect(function()
+			local v9 = l__Maid__1.new();
+			v9:GiveTask(p1.vendingMachine:GetAttributeChangedSignal("VendingMachineState"):Connect(function()
+				v4(p1.vendingMachine:GetAttribute("VendingMachineState"));
+			end));
+			v9:GiveTask(p1.vendingMachine:GetAttributeChangedSignal("ChoosenReward"):Connect(function()
+				v6(p1.vendingMachine:GetAttribute("ChoosenReward"));
+			end));
+			v9:GiveTask(p1.vendingMachine:GetAttributeChangedSignal("VendingMachineDispenseTime"):Connect(function()
+				v8(p1.vendingMachine:GetAttribute("VendingMachineDispenseTime"));
+			end));
+			return function()
+				v9:DoCleaning();
+			end;
+		end, {});
+		return u2.createElement(l__VendingMachineApp__3, {
+			vendingMachineState = v3, 
+			choosenReward = v5, 
+			soundPosition = p1.vendingMachine.Position, 
+			dispenseTime = v7
+		});
+	end)
+};
