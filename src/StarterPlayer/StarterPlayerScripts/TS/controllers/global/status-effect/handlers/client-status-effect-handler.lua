e40bbd600ac1d9ed5b163ba809916894a246392c
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:37:02 +0800

    Update

---
+++ src/StarterPlayer/StarterPlayerScripts/TS/controllers/global/status-effect/handlers/client-status-effect-handler.lua
@@ -0,0 +1,18 @@
+-- Decompiled with the Synapse X Luau decompiler.
+
+local v1 = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
+local v2 = {};
+local u1 = v1.import(script, v1.getModule(script, "@rbxts", "maid").Maid);
+function v2.constructor(p1)
+	p1.maid = u1.new();
+end;
+function v2.apply(p2, p3, p4)
+	p2:onApply(p3, p4);
+end;
+function v2.remove(p5, p6, p7)
+	p5.maid:DoCleaning();
+	p5:onRemove(p6, p7);
+end;
+return {
+	ClientStatusEffectHandler = v2
+};
