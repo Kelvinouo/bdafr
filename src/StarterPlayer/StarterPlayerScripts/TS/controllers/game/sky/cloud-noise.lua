a90c091f41d3986343f7cb2f9eda76d653fe3bb0
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:36:00 +0800

    Update

---
+++ src/StarterPlayer/StarterPlayerScripts/TS/controllers/game/sky/cloud-noise.lua
@@ -0,0 +1,803 @@
+-- Decompiled with the Synapse X Luau decompiler.
+
+return {
+	CloudNoise = { {
+			[9] = true, 
+			[10] = true, 
+			[11] = true, 
+			[12] = true, 
+			[29] = true, 
+			[30] = true, 
+			[31] = true
+		}, {
+			[28] = true, 
+			[29] = true, 
+			[30] = true, 
+			[31] = true
+		}, {
+			[2] = true, 
+			[3] = true, 
+			[6] = true, 
+			[7] = true, 
+			[28] = true, 
+			[29] = true, 
+			[42] = true, 
+			[43] = true, 
+			[44] = true, 
+			[45] = true
+		}, {
+			[27] = true, 
+			[28] = true, 
+			[29] = true, 
+			[42] = true, 
+			[43] = true, 
+			[44] = true, 
+			[45] = true,
+			true, true, true
+		}, {
+			[10] = true, 
+			[11] = true, 
+			[58] = true, 
+			[59] = true, 
+			[60] = true, 
+			[61] = true, 
+			[67] = true,
+			true
+		}, {
+			[8] = true, 
+			[9] = true, 
+			[10] = true, 
+			[11] = true, 
+			[59] = true, 
+			[60] = true, 
+			[61] = true, 
+			[67] = true
+		}, {
+			[20] = true, 
+			[59] = true, 
+			[60] = true, 
+			[61] = true, 
+			[67] = true, 
+			[68] = true, 
+			[69] = true, 
+			[70] = true, 
+			[71] = true, 
+			[72] = true
+		}, {
+			[19] = true, 
+			[20] = true, 
+			[59] = true, 
+			[60] = true
+		}, {
+			[8] = true, 
+			[9] = true, 
+			[14] = true, 
+			[15] = true, 
+			[19] = true, 
+			[20] = true, 
+			[32] = true, 
+			[33] = true, 
+			[44] = true, 
+			[45] = true, 
+			[46] = true, 
+			[52] = true, 
+			[53] = true
+		}, {
+			[8] = true, 
+			[9] = true, 
+			[14] = true, 
+			[15] = true, 
+			[32] = true, 
+			[33] = true, 
+			[52] = true, 
+			[53] = true, 
+			[77] = true, 
+			[78] = true
+		}, {
+			[32] = true, 
+			[33] = true, 
+			[77] = true, 
+			[78] = true
+		}, {
+			[32] = true, 
+			[33] = true, 
+			[77] = true
+		}, {
+			[30] = true, 
+			[31] = true, 
+			[32] = true, 
+			[33] = true, 
+			[39] = true, 
+			[40] = true, 
+			[68] = true, 
+			[69] = true, 
+			[70] = true, 
+			[71] = true
+		}, {
+			[30] = true, 
+			[31] = true, 
+			[32] = true, 
+			[33] = true, 
+			[39] = true, 
+			[40] = true, 
+			[51] = true, 
+			[52] = true, 
+			[68] = true, 
+			[69] = true, 
+			[70] = true, 
+			[71] = true
+		}, {
+			[5] = true, 
+			[6] = true, 
+			[30] = true, 
+			[31] = true, 
+			[39] = true, 
+			[40] = true, 
+			[50] = true, 
+			[51] = true, 
+			[52] = true, 
+			[70] = true, 
+			[71] = true
+		}, {
+			[5] = true, 
+			[6] = true, 
+			[19] = true, 
+			[20] = true, 
+			[30] = true, 
+			[31] = true, 
+			[39] = true, 
+			[40] = true, 
+			[50] = true, 
+			[51] = true, 
+			[52] = true, 
+			[70] = true, 
+			[71] = true
+		}, {
+			[5] = true, 
+			[12] = true, 
+			[13] = true, 
+			[19] = true, 
+			[20] = true, 
+			[39] = true, 
+			[50] = true, 
+			[51] = true, 
+			[60] = true, 
+			[61] = true
+		}, {
+			[12] = true, 
+			[13] = true, 
+			[60] = true, 
+			[61] = true
+		}, {
+			[11] = true, 
+			[12] = true, 
+			[26] = true, 
+			[27] = true, 
+			[28] = true, 
+			[29] = true, 
+			[60] = true, 
+			[61] = true, 
+			[68] = true, 
+			[69] = true, 
+			[74] = true, 
+			[75] = true
+		}, {
+			[11] = true, 
+			[12] = true, 
+			[26] = true, 
+			[27] = true, 
+			[28] = true, 
+			[29] = true, 
+			[60] = true, 
+			[61] = true, 
+			[68] = true, 
+			[69] = true, 
+			[74] = true, 
+			[75] = true, 
+			[76] = true
+		}, {
+			[11] = true, 
+			[12] = true, 
+			[26] = true, 
+			[27] = true, 
+			[28] = true, 
+			[29] = true, 
+			[52] = true, 
+			[53] = true, 
+			[60] = true, 
+			[68] = true, 
+			[69] = true, 
+			[74] = true, 
+			[75] = true, 
+			[76] = true
+		}, {
+			[21] = true, 
+			[22] = true, 
+			[26] = true, 
+			[27] = true, 
+			[28] = true, 
+			[29] = true, 
+			[52] = true, 
+			[53] = true, 
+			[68] = true, 
+			[69] = true, 
+			[75] = true, 
+			[76] = true
+		}, {
+			[3] = true, 
+			[4] = true, 
+			[5] = true, 
+			[22] = true, 
+			[23] = true, 
+			[36] = true, 
+			[37] = true, 
+			[52] = true, 
+			[53] = true, 
+			[75] = true, 
+			[76] = true
+		}, {
+			[4] = true, 
+			[5] = true, 
+			[36] = true, 
+			[37] = true, 
+			[42] = true, 
+			[43] = true, 
+			[52] = true, 
+			[53] = true, 
+			[76] = true
+		}, {
+			[4] = true, 
+			[5] = true, 
+			[16] = true, 
+			[17] = true, 
+			[42] = true, 
+			[43] = true, 
+			[44] = true, 
+			[45] = true, 
+			[46] = true, 
+			[47] = true, 
+			[64] = true, 
+			[65] = true
+		}, {
+			[4] = true, 
+			[5] = true, 
+			[16] = true, 
+			[17] = true, 
+			[18] = true, 
+			[19] = true, 
+			[20] = true, 
+			[42] = true, 
+			[43] = true, 
+			[44] = true, 
+			[45] = true, 
+			[46] = true, 
+			[47] = true, 
+			[63] = true, 
+			[64] = true, 
+			[65] = true
+		}, {
+			[17] = true, 
+			[18] = true, 
+			[19] = true, 
+			[20] = true, 
+			[46] = true, 
+			[47] = true, 
+			[48] = true, 
+			[49] = true, 
+			[56] = true, 
+			[57] = true, 
+			[64] = true, 
+			[65] = true, 
+			[72] = true, 
+			[73] = true
+		}, {
+			[18] = true, 
+			[46] = true, 
+			[47] = true, 
+			[48] = true, 
+			[49] = true, 
+			[56] = true, 
+			[57] = true, 
+			[58] = true, 
+			[59] = true, 
+			[64] = true, 
+			[65] = true, 
+			[72] = true, 
+			[73] = true
+		}, {
+			[0] = true, 
+			[27] = true, 
+			[28] = true, 
+			[56] = true, 
+			[57] = true,
+			true
+		}, {
+			[0] = true, 
+			[27] = true, 
+			[28] = true, 
+			[29] = true, 
+			[55] = true, 
+			[56] = true, 
+			[57] = true,
+			true
+		}, {
+			[0] = true, 
+			[11] = true, 
+			[12] = true, 
+			[13] = true, 
+			[34] = true,
+			true
+		}, {
+			[0] = true, 
+			[12] = true, 
+			[13] = true, 
+			[34] = true, 
+			[47] = true, 
+			[48] = true,
+			true
+		}, {
+			[6] = true, 
+			[7] = true, 
+			[33] = true, 
+			[34] = true, 
+			[44] = true, 
+			[45] = true, 
+			[46] = true, 
+			[62] = true, 
+			[63] = true
+		}, {
+			[6] = true, 
+			[7] = true, 
+			[44] = true, 
+			[45] = true, 
+			[62] = true, 
+			[63] = true
+		}, {
+			[4] = true, 
+			[5] = true, 
+			[42] = true, 
+			[43] = true
+		}, {
+			[4] = true, 
+			[5] = true
+		}, {
+			[2] = true, 
+			[3] = true, 
+			[20] = true, 
+			[21] = true, 
+			[28] = true, 
+			[29] = true, 
+			[54] = true, 
+			[55] = true, 
+			[56] = true, 
+			[57] = true, 
+			[58] = true, 
+			[59] = true, 
+			[76] = true, 
+			[77] = true, 
+			[78] = true, 
+			[79] = true
+		}, {
+			[2] = true, 
+			[3] = true, 
+			[20] = true, 
+			[21] = true, 
+			[28] = true, 
+			[29] = true, 
+			[54] = true, 
+			[55] = true, 
+			[56] = true, 
+			[57] = true, 
+			[58] = true, 
+			[59] = true, 
+			[76] = true, 
+			[77] = true, 
+			[78] = true, 
+			[79] = true
+		}, {
+			[4] = true, 
+			[5] = true, 
+			[20] = true, 
+			[21] = true, 
+			[28] = true, 
+			[29] = true, 
+			[36] = true, 
+			[37] = true, 
+			[52] = true, 
+			[53] = true
+		}, {
+			[4] = true, 
+			[5] = true, 
+			[20] = true, 
+			[21] = true, 
+			[28] = true, 
+			[29] = true, 
+			[36] = true, 
+			[37] = true, 
+			[52] = true, 
+			[53] = true, 
+			[66] = true, 
+			[67] = true, 
+			[68] = true
+		}, {
+			[14] = true, 
+			[15] = true, 
+			[34] = true, 
+			[35] = true, 
+			[67] = true, 
+			[68] = true
+		}, {
+			[14] = true, 
+			[15] = true, 
+			[34] = true, 
+			[35] = true, 
+			[67] = true, 
+			[68] = true
+		}, {
+			[32] = true, 
+			[33] = true, 
+			[34] = true, 
+			[35] = true, 
+			[67] = true, 
+			[68] = true
+		}, {
+			[11] = true, 
+			[32] = true, 
+			[33] = true, 
+			[34] = true, 
+			[35] = true, 
+			[43] = true, 
+			[44] = true, 
+			[45] = true, 
+			[46] = true
+		}, {
+			[0] = true, 
+			[10] = true, 
+			[11] = true, 
+			[12] = true, 
+			[13] = true, 
+			[45] = true, 
+			[46] = true,
+			true
+		}, {
+			[0] = true, 
+			[10] = true, 
+			[11] = true, 
+			[12] = true, 
+			[13] = true, 
+			[52] = true, 
+			[53] = true, 
+			[54] = true, 
+			[55] = true, 
+			[56] = true, 
+			[73] = true, 
+			[74] = true, 
+			[75] = true, 
+			[76] = true,
+			true
+		}, {
+			[0] = true, 
+			[12] = true, 
+			[32] = true, 
+			[33] = true, 
+			[51] = true, 
+			[52] = true, 
+			[53] = true, 
+			[54] = true, 
+			[55] = true, 
+			[64] = true, 
+			[65] = true, 
+			[73] = true, 
+			[74] = true, 
+			[75] = true, 
+			[76] = true
+		}, {
+			[12] = true, 
+			[19] = true, 
+			[20] = true, 
+			[21] = true, 
+			[22] = true, 
+			[32] = true, 
+			[33] = true, 
+			[52] = true, 
+			[53] = true, 
+			[54] = true, 
+			[64] = true, 
+			[65] = true
+		}, {
+			[8] = true, 
+			[9] = true, 
+			[19] = true, 
+			[20] = true, 
+			[21] = true, 
+			[22] = true, 
+			[30] = true, 
+			[31] = true, 
+			[32] = true, 
+			[33] = true
+		}, {
+			[7] = true, 
+			[8] = true, 
+			[9] = true, 
+			[21] = true, 
+			[30] = true, 
+			[31] = true, 
+			[32] = true, 
+			[33] = true
+		}, {
+			[7] = true, 
+			[8] = true, 
+			[9] = true, 
+			[44] = true, 
+			[45] = true, 
+			[68] = true, 
+			[69] = true
+		}, {
+			[7] = true, 
+			[8] = true, 
+			[9] = true, 
+			[44] = true, 
+			[45] = true, 
+			[68] = true, 
+			[69] = true
+		}, {
+			[40] = true, 
+			[41] = true, 
+			[44] = true, 
+			[45] = true, 
+			[48] = true, 
+			[49] = true, 
+			[74] = true, 
+			[75] = true
+		}, {
+			[38] = true, 
+			[40] = true, 
+			[41] = true, 
+			[44] = true, 
+			[45] = true, 
+			[48] = true, 
+			[49] = true, 
+			[74] = true
+		}, {
+			[36] = true, 
+			[37] = true, 
+			[56] = true, 
+			[57] = true, 
+			[64] = true, 
+			[65] = true, 
+			[70] = true, 
+			[71] = true, 
+			[72] = true, 
+			[73] = true, 
+			[74] = true
+		}, {
+			[36] = true, 
+			[37] = true, 
+			[56] = true, 
+			[57] = true, 
+			[64] = true, 
+			[65] = true, 
+			[70] = true, 
+			[71] = true, 
+			[72] = true
+		}, {
+			[16] = true, 
+			[17] = true, 
+			[36] = true, 
+			[37] = true, 
+			[56] = true, 
+			[57] = true, 
+			[64] = true, 
+			[65] = true, 
+			[72] = true
+		}, {
+			[11] = true, 
+			[12] = true, 
+			[16] = true, 
+			[17] = true, 
+			[28] = true, 
+			[35] = true, 
+			[36] = true, 
+			[37] = true, 
+			[56] = true, 
+			[57] = true, 
+			[64] = true, 
+			[65] = true, 
+			[72] = true
+		}, {
+			[11] = true, 
+			[26] = true, 
+			[27] = true, 
+			[28] = true, 
+			[29] = true, 
+			[60] = true, 
+			[61] = true
+		}, {
+			[6] = true, 
+			[7] = true, 
+			[26] = true, 
+			[27] = true, 
+			[28] = true, 
+			[29] = true, 
+			[30] = true, 
+			[60] = true, 
+			[61] = true
+		}, {
+			[6] = true, 
+			[7] = true, 
+			[27] = true, 
+			[28] = true, 
+			[29] = true
+		}, {
+			[6] = true, 
+			[7] = true, 
+			[8] = true, 
+			[27] = true, 
+			[28] = true, 
+			[29] = true
+		}, {
+			[7] = true, 
+			[8] = true, 
+			[36] = true, 
+			[37] = true
+		}, {
+			[7] = true, 
+			[8] = true, 
+			[9] = true, 
+			[36] = true, 
+			[37] = true
+		}, {
+			[0] = true, 
+			[8] = true, 
+			[9] = true, 
+			[16] = true, 
+			[17] = true, 
+			[26] = true, 
+			[27] = true, 
+			[36] = true, 
+			[37] = true, 
+			[50] = true, 
+			[51] = true, 
+			[52] = true, 
+			[53] = true, 
+			[54] = true, 
+			[55] = true,
+			true, true, true
+		}, {
+			[0] = true, 
+			[16] = true, 
+			[17] = true, 
+			[26] = true, 
+			[27] = true, 
+			[36] = true, 
+			[37] = true, 
+			[50] = true, 
+			[51] = true, 
+			[52] = true, 
+			[53] = true, 
+			[54] = true, 
+			[55] = true,
+			true, true, true
+		}, {
+			[16] = true, 
+			[17] = true, 
+			[52] = true, 
+			[53] = true, 
+			[64] = true, 
+			[65] = true
+		}, {
+			[16] = true, 
+			[17] = true, 
+			[52] = true, 
+			[53] = true, 
+			[64] = true, 
+			[65] = true, 
+			[73] = true, 
+			[74] = true
+		}, {
+			[14] = true, 
+			[15] = true, 
+			[20] = true, 
+			[21] = true, 
+			[42] = true, 
+			[43] = true, 
+			[44] = true, 
+			[45] = true, 
+			[73] = true, 
+			[74] = true
+		}, {
+			[9] = true, 
+			[10] = true, 
+			[14] = true, 
+			[15] = true, 
+			[20] = true, 
+			[21] = true, 
+			[42] = true, 
+			[43] = true, 
+			[44] = true, 
+			[45] = true, 
+			[73] = true, 
+			[74] = true
+		}, {
+			[10] = true, 
+			[20] = true, 
+			[21] = true, 
+			[72] = true, 
+			[73] = true, 
+			[74] = true
+		}, {
+			[20] = true, 
+			[21] = true, 
+			[56] = true, 
+			[57] = true, 
+			[58] = true, 
+			[72] = true, 
+			[73] = true, 
+			[74] = true
+		}, {
+			[9] = true, 
+			[10] = true, 
+			[29] = true, 
+			[30] = true, 
+			[31] = true, 
+			[37] = true, 
+			[56] = true, 
+			[57] = true, 
+			[58] = true
+		}, {
+			[9] = true, 
+			[10] = true, 
+			[29] = true, 
+			[30] = true, 
+			[31] = true, 
+			[36] = true, 
+			[37] = true, 
+			[38] = true
+		}, {
+			[29] = true, 
+			[30] = true, 
+			[31] = true, 
+			[38] = true, 
+			[65] = true, 
+			[66] = true, 
+			[67] = true, 
+			[68] = true
+		}, {
+			[9] = true, 
+			[10] = true, 
+			[45] = true, 
+			[46] = true, 
+			[47] = true, 
+			[48] = true, 
+			[49] = true, 
+			[50] = true, 
+			[65] = true, 
+			[66] = true, 
+			[67] = true, 
+			[68] = true
+		}, {
+			[2] = true, 
+			[3] = true, 
+			[4] = true, 
+			[5] = true, 
+			[10] = true, 
+			[48] = true, 
+			[49] = true, 
+			[50] = true
+		}, {
+			[2] = true, 
+			[3] = true, 
+			[4] = true, 
+			[5] = true
+		}, {
+			[0] = true, 
+			[20] = true, 
+			[21] = true,
+			true, true, true
+		}, {
+			[0] = true, 
+			[20] = true, 
+			[21] = true,
+			true, true, true
+		} }
+};
