c864552f6064a21ec78c080099d351a16d987a62
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:36:02 +0800

    Update

---
+++ src/StarterPlayer/StarterPlayerScripts/TS/controllers/game/locker/kill-effect/kill-effect/story.lua
@@ -0,0 +1,16 @@
+-- Decompiled with the Synapse X Luau decompiler.
+
+local v1 = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
+local u1 = v1.import(script, v1.getModule(script, "@rbxts", "maid").Maid);
+local l__RunService__2 = v1.import(script, v1.getModule(script, "@rbxts", "services")).RunService;
+return function(p1)
+	local v2 = u1.new();
+	v2:GiveTask(l__RunService__2.RenderStepped:Connect(function()
+		if os.clock() - -1 > 8 then
+
+		end;
+	end));
+	return function()
+		v2:DoCleaning();
+	end;
+end;
