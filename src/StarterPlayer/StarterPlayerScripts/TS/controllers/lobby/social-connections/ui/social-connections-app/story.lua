8056ce098aa8df9a70eca961b2341bff205c690b
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:33:42 +0800

    Update

--- src/StarterPlayer/StarterPlayerScripts/TS/controllers/lobby/social-connections/ui/social-connections-app/story.lua
+++ src/StarterPlayer/StarterPlayerScripts/TS/controllers/lobby/social-connections/ui/social-connections-app/story.lua
@@ -1,1 +1,28 @@
-Hello 5
+-- Decompiled with the Synapse X Luau decompiler.
+
+local v1 = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
+local l__ClientStore__1 = v1.import(script, script.Parent.Parent.Parent.Parent.Parent, "ui", "store").ClientStore;
+local l__CreateRoduxApp__2 = v1.import(script, script.Parent.Parent.Parent.Parent.Parent, "ui", "rodux", "create-rodux-app").CreateRoduxApp;
+local l__SocialConnectionsCore__3 = v1.import(script, script.Parent, "social-connections-core").SocialConnectionsCore;
+local u4 = v1.import(script, v1.getModule(script, "@rbxts", "roact").src);
+return function(p1)
+	l__ClientStore__1:dispatch({
+		type = "SocialConnectionsSetAll", 
+		supportTasks = {
+			joinedGroup = false
+		}, 
+		connections = {
+			discord = {
+				verificationCode = "A3K4LL1", 
+				validated = false, 
+				legacyDiscordVerification = false
+			}
+		}
+	});
+	local u5 = l__CreateRoduxApp__2("SocialConnectionsApp", l__SocialConnectionsCore__3, {}, {}, {
+		Parent = p1
+	});
+	return function()
+		return u4.unmount(u5);
+	end;
+end;
