dc06fa41fcf1646eb1fa4517338d9f56ad437818
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:39:43 +0800

    Update

---
+++ src/Players/IIIlIlIllllllIIlllIl/PlayerScripts/TS/controllers/lobby/prophunt/prophunt-controller.lua
@@ -0,0 +1,28 @@
+-- Decompiled with the Synapse X Luau decompiler.
+
+local v1 = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
+local l__KnitController__2 = v1.import(script, script.Parent.Parent.Parent.Parent, "lib", "knit", "knit-controller").KnitController;
+local v3 = setmetatable({}, {
+	__tostring = function()
+		return "ProphuntController";
+	end, 
+	__index = l__KnitController__2
+});
+v3.__index = v3;
+function v3.new(...)
+	local v4 = setmetatable({}, v3);
+	return v4:constructor(...) and v4;
+end;
+local u1 = l__KnitController__2;
+function v3.constructor(p1)
+	u1.constructor(p1);
+	p1.Name = "ProphuntController";
+end;
+function v3.KnitStart(p2)
+	u1.KnitStart(p2);
+	local v5 = CFrame.new(19.732, 301.766, 196.907);
+	local v6 = CFrame.new(20.287, 301.369, 181.164) * CFrame.Angles(0, math.pi, 0);
+end;
+u1 = v1.import(script, v1.getModule(script, "@rbxts", "knit").src).KnitClient.CreateController;
+u1 = u1(v3.new());
+return nil;
