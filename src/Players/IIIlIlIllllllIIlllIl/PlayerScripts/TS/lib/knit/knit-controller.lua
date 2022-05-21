4837ca4095329258fec8f1970d6ef0c317e65694
Fern <58261438+Kelvinouo@users.noreply.github.com>
2022-05-21 11:42:10 +0800

    Update

---
+++ src/Players/IIIlIlIllllllIIlllIl/PlayerScripts/TS/lib/knit/knit-controller.lua
@@ -0,0 +1,12 @@
+-- Decompiled with the Synapse X Luau decompiler.
+
+return {
+	KnitController = {
+		constructor = function(p1)
+			p1.Name = "Controller";
+		end, 
+		KnitStart = function(p2)
+
+		end
+	}
+};
