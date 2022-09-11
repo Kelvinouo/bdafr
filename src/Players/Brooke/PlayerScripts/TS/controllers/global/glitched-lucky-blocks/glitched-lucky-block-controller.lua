6e0d2c4d44d96920a68b07b0ed0e7fcf4f6e5ae7
Fern <915642604cc@gmail.com>
2022-09-11 21:04:22 +0800

    11092022_01

--- src/Players/Brooke/PlayerScripts/TS/controllers/global/glitched-lucky-blocks/glitched-lucky-block-controller.lua
+++ src/Players/Brooke/PlayerScripts/TS/controllers/global/glitched-lucky-blocks/glitched-lucky-block-controller.lua
@@ -22,75 +22,78 @@ end;
 local l__KnitClient__1 = v1.import(script, v1.getModule(script, "@easy-games", "knit").src).KnitClient;
 local l__ItemType__2 = v1.import(script, game:GetService("ReplicatedStorage"), "TS", "item", "item-type").ItemType;
 local l__GameSound__3 = v1.import(script, game:GetService("ReplicatedStorage"), "TS", "sound", "game-sound").GameSound;
-local l__default__4 = v1.import(script, game:GetService("ReplicatedStorage"), "TS", "remotes").default;
-local u5 = v1.import(script, v1.getModule(script, "@rbxts", "roact").src);
-local u6 = v1.import(script, script.Parent, "glitched-match-announcement").GlitchedMatchAnnouncement;
-local l__Players__7 = v3.Players;
-local l__BalanceFile__8 = v1.import(script, game:GetService("ReplicatedStorage"), "TS", "balance", "balance-file").BalanceFile;
-local l__SoundManager__9 = v2.SoundManager;
-local l__WatchCollectionTag__10 = v2.WatchCollectionTag;
-local l__LuckyBlockCategory__11 = v1.import(script, game:GetService("ReplicatedStorage"), "TS", "lucky-block", "lucky-block-enums").LuckyBlockCategory;
-local l__ClientSyncEvents__12 = v1.import(script, script.Parent.Parent.Parent.Parent, "client-sync-events").ClientSyncEvents;
+local l__PlaceUtil__4 = v1.import(script, game:GetService("ReplicatedStorage"), "TS", "util", "place-util").PlaceUtil;
+local l__default__5 = v1.import(script, game:GetService("ReplicatedStorage"), "TS", "remotes").default;
+local u6 = v1.import(script, v1.getModule(script, "@rbxts", "roact").src);
+local u7 = v1.import(script, script.Parent, "glitched-match-announcement").GlitchedMatchAnnouncement;
+local l__Players__8 = v3.Players;
+local l__BalanceFile__9 = v1.import(script, game:GetService("ReplicatedStorage"), "TS", "balance", "balance-file").BalanceFile;
+local l__SoundManager__10 = v2.SoundManager;
+local l__WatchCollectionTag__11 = v2.WatchCollectionTag;
+local l__LuckyBlockCategory__12 = v1.import(script, game:GetService("ReplicatedStorage"), "TS", "lucky-block", "lucky-block-enums").LuckyBlockCategory;
+local l__ClientSyncEvents__13 = v1.import(script, script.Parent.Parent.Parent.Parent, "client-sync-events").ClientSyncEvents;
 function v5.KnitStart(p2)
 	l__KnitController__4.KnitStart(p2);
 	l__KnitClient__1.Controllers.PreloadController:preloadForItemType(l__ItemType__2.GLITCHED_LUCKY_BLOCK, {
 		sounds = { l__GameSound__3.TV_STATIC, l__GameSound__3.GLITCHED_LUCKY_BLOCK_DAMAGE, l__GameSound__3.GLITCHED_LUCKY_BLOCK_TELEPORT, l__GameSound__3.GLITCH_OVERLAY, l__GameSound__3.GLITCH_OVERLAY_2 }
 	});
-	l__default__4.Client:WaitFor("RemoteName"):expect():Connect(function()
-		local u13 = u5.mount(u5.createElement("ScreenGui", {}, { u5.createElement(u6) }), l__Players__7.LocalPlayer:FindFirstChildOfClass("PlayerGui"));
-		task.delay(l__BalanceFile__8.GLITCH_LB_ANNOUNCEMENT_DURATION, function()
-			l__SoundManager__9:playSound(l__GameSound__3.GLITCH_OVERLAY_2, {
-				volumeMultiplier = 0.7
-			});
-			u5.unmount(u13);
+	if l__PlaceUtil__4.isGameServer() then
+		l__default__5.Client:OnEvent("RemoteName", function()
+			local u14 = u6.mount(u6.createElement("ScreenGui", {}, { u6.createElement(u7) }), l__Players__8.LocalPlayer:FindFirstChildOfClass("PlayerGui"));
+			task.delay(l__BalanceFile__9.GLITCH_LB_ANNOUNCEMENT_DURATION, function()
+				l__SoundManager__10:playSound(l__GameSound__3.GLITCH_OVERLAY_2, {
+					volumeMultiplier = 0.7
+				});
+				u6.unmount(u14);
+			end);
 		end);
-	end);
-	l__WatchCollectionTag__10("GlitchedLuckyBlock", function(p3)
+	end;
+	l__WatchCollectionTag__11("GlitchedLuckyBlock", function(p3)
 		p2:playGlitchOverlayEffect(p3);
 		p2:loopGlitchParticlesOnBlock(p3);
 	end);
-	l__default__4.Client:OnEvent("RemoteName", function(p4)
-		if p4.category ~= l__LuckyBlockCategory__11.GLITCHED then
+	l__default__5.Client:OnEvent("RemoteName", function(p4)
+		if p4.category ~= l__LuckyBlockCategory__12.GLITCHED then
 			return nil;
 		end;
-		l__SoundManager__9:playSound(l__GameSound__3.GLITCHED_LUCKY_BLOCK_DAMAGE);
+		l__SoundManager__10:playSound(l__GameSound__3.GLITCHED_LUCKY_BLOCK_DAMAGE);
 		p2:playGlitchParticlesOnBlock(p4.blockPosition);
 	end);
-	l__ClientSyncEvents__12.PlaceBlock:connect(function(p5)
+	l__ClientSyncEvents__13.PlaceBlock:connect(function(p5)
 		if p5.blockType ~= l__ItemType__2.GLITCHED_LUCKY_BLOCK then
 			return nil;
 		end;
 		p2:playGlitchParticlesOnBlock(p5.blockPosition);
-		l__SoundManager__9:playSound(l__GameSound__3.TV_STATIC, {
+		l__SoundManager__10:playSound(l__GameSound__3.TV_STATIC, {
 			playbackSpeedMultiplier = math.random(0.5, 1.5), 
 			volumeMultiplier = 0.05
 		});
 	end);
-	l__ClientSyncEvents__12.DamageBlock:connect(function(p6)
+	l__ClientSyncEvents__13.DamageBlock:connect(function(p6)
 		if p6.blockType ~= l__ItemType__2.GLITCHED_LUCKY_BLOCK then
 			return nil;
 		end;
-		l__SoundManager__9:playSound(l__GameSound__3.TV_STATIC, {
+		l__SoundManager__10:playSound(l__GameSound__3.TV_STATIC, {
 			playbackSpeedMultiplier = math.random(0.5, 1.5), 
 			volumeMultiplier = 0.05
 		});
-		l__SoundManager__9:playSound(l__GameSound__3.GLITCHED_LUCKY_BLOCK_DAMAGE);
+		l__SoundManager__10:playSound(l__GameSound__3.GLITCHED_LUCKY_BLOCK_DAMAGE);
 		p2:playGlitchParticlesOnBlock(p6.blockPosition);
 	end);
-	l__default__4.Client:OnEvent("RemoteName", function(p7)
-		l__SoundManager__9:playSound(l__GameSound__3.GLITCHED_LUCKY_BLOCK_DAMAGE);
+	l__default__5.Client:OnEvent("RemoteName", function(p7)
+		l__SoundManager__10:playSound(l__GameSound__3.GLITCHED_LUCKY_BLOCK_DAMAGE);
 		p2:playGlitchedLuckyBlockCloneEffect(p7.initialPosition, true);
-		wait(l__BalanceFile__8.GLITCH_LB_TELEPORT_DURATION);
-		l__SoundManager__9:playSound(l__GameSound__3.GLITCHED_LUCKY_BLOCK_TELEPORT);
+		wait(l__BalanceFile__9.GLITCH_LB_TELEPORT_DURATION);
+		l__SoundManager__10:playSound(l__GameSound__3.GLITCHED_LUCKY_BLOCK_TELEPORT);
 		p2:playGlitchedLuckyBlockCloneEffect(p7.newPosition, false);
 	end);
 end;
-local u14 = v1.import(script, v1.getModule(script, "@rbxts", "make"));
-local l__SpriteSheetPlayer__15 = v2.SpriteSheetPlayer;
-local l__RandomUtil__16 = v2.RandomUtil;
+local u15 = v1.import(script, v1.getModule(script, "@rbxts", "make"));
+local l__SpriteSheetPlayer__16 = v2.SpriteSheetPlayer;
+local l__RandomUtil__17 = v2.RandomUtil;
 function v5.playGlitchOverlayEffect(p8, p9)
 	local v7 = Enum.NormalId:GetEnumItems();
-	local u17 = { {
+	local u18 = { {
 			image = "rbxassetid://10844183211", 
 			imageWidth = 1024, 
 			imageHeight = 769, 
@@ -113,20 +116,20 @@ function v5.playGlitchOverlayEffect(p8, p9)
 				loopDelay = 5
 			}
 		} };
-	local u18 = Random.new();
+	local u19 = Random.new();
 	local function v8(p10)
-		local u19 = l__SpriteSheetPlayer__15.new(u14("ImageLabel", {
+		local u20 = l__SpriteSheetPlayer__16.new(u15("ImageLabel", {
 			Size = UDim2.fromScale(1, 1), 
 			BackgroundTransparency = 1, 
-			Parent = u14("SurfaceGui", {
+			Parent = u15("SurfaceGui", {
 				Name = "SpriteSheet_Player_" .. p10.Name, 
 				Face = p10, 
 				Brightness = 3, 
 				Parent = p9
 			})
-		}), l__RandomUtil__16.randomArraySelectN(u17, 1)[1]);
-		task.delay(u18:NextNumber(0, 2), function()
-			u19:play();
+		}), l__RandomUtil__17.randomArraySelectN(u18, 1)[1]);
+		task.delay(u19:NextNumber(0, 2), function()
+			u20:play();
 		end);
 	end;
 	local v9, v10, v11 = ipairs(v7);
@@ -139,7 +142,7 @@ function v5.playGlitchOverlayEffect(p8, p9)
 		v8(v10, v9 - 1, v7);	
 	end;
 	local v12 = Enum.NormalId:GetEnumItems();
-	local u20 = u14("Part", {
+	local u21 = u15("Part", {
 		Name = "Floating_Pixels_Layer", 
 		Size = Vector3.new(3.2, 3.2, 3.2), 
 		Position = p9.Position, 
@@ -150,15 +153,15 @@ function v5.playGlitchOverlayEffect(p8, p9)
 		Parent = p9
 	});
 	local function v13(p11)
-		local v14 = u14("SurfaceGui", {
+		local v14 = u15("SurfaceGui", {
 			Name = "Floating_Layer_" .. p11.Name, 
 			Face = p11, 
-			Parent = u20, 
+			Parent = u21, 
 			SizingMode = Enum.SurfaceGuiSizingMode.PixelsPerStud, 
 			PixelsPerStud = 3
 		});
 		local v15 = { Color3.fromHex("#3ca7bf"), Color3.fromHex("#62197c"), Color3.fromHex("#163050"), Color3.fromHex("#1e546f"), Color3.fromHex("#44125d"), Color3.fromHex("#26073e"), Color3.fromHex("#f2f2fe") };
-		local v16 = u18:NextInteger(2, 4);
+		local v16 = u19:NextInteger(2, 4);
 		local v17 = 0;
 		local v18 = false;
 		while true do
@@ -170,10 +173,10 @@ function v5.playGlitchOverlayEffect(p8, p9)
 			if not (v17 < v16) then
 				break;
 			end;
-			local v19 = u14("ImageLabel", {
-				Size = UDim2.new(0, u18:NextInteger(1, 3), 0, 1), 
-				Position = UDim2.fromOffset(u18:NextInteger(0, 8), u18:NextInteger(0, 8)), 
-				BackgroundColor3 = v15[u18:NextInteger(0, #v15 - 1) + 1], 
+			local v19 = u15("ImageLabel", {
+				Size = UDim2.new(0, u19:NextInteger(1, 3), 0, 1), 
+				Position = UDim2.fromOffset(u19:NextInteger(0, 8), u19:NextInteger(0, 8)), 
+				BackgroundColor3 = v15[u19:NextInteger(0, #v15 - 1) + 1], 
 				BorderSizePixel = 0, 
 				Parent = v14
 			});		
@@ -189,17 +192,17 @@ function v5.playGlitchOverlayEffect(p8, p9)
 		v13(v21, v20 - 1, v12);	
 	end;
 end;
-local l__ReplicatedStorage__21 = v3.ReplicatedStorage;
-local l__Workspace__22 = v3.Workspace;
-local l__BlockEngine__23 = v1.import(script, v1.getModule(script, "@easy-games", "block-engine").out).BlockEngine;
-local l__EffectUtil__24 = v1.import(script, script.Parent.Parent.Parent.Parent, "lib", "effect", "effect-util").EffectUtil;
+local l__ReplicatedStorage__22 = v3.ReplicatedStorage;
+local l__Workspace__23 = v3.Workspace;
+local l__BlockEngine__24 = v1.import(script, v1.getModule(script, "@easy-games", "block-engine").out).BlockEngine;
+local l__EffectUtil__25 = v1.import(script, script.Parent.Parent.Parent.Parent, "lib", "effect", "effect-util").EffectUtil;
 function v5.playGlitchParticlesOnBlock(p12, p13, p14, p15)
 	task.spawn(function()
-		local v23 = l__ReplicatedStorage__21.Assets.Effects.GlitchedLuckyBlockEffect:Clone();
+		local v23 = l__ReplicatedStorage__22.Assets.Effects.GlitchedLuckyBlockEffect:Clone();
 		v23.Name = "GlitchedLuckyBlockEffect";
-		v23.Parent = l__Workspace__22;
+		v23.Parent = l__Workspace__23;
 		v23.Transparency = 1;
-		v23.CFrame = CFrame.new((l__BlockEngine__23:getWorldPosition(p13)));
+		v23.CFrame = CFrame.new((l__BlockEngine__24:getWorldPosition(p13)));
 		local v24 = {
 			destroyAfterSec = 1.5
 		};
@@ -213,20 +216,20 @@ function v5.playGlitchParticlesOnBlock(p12, p13, p14, p15)
 			v26 = 1.3;
 		end;
 		v24.particleMultiplier = v26;
-		l__EffectUtil__24:playEffects({ v23 }, nil, v24);
+		l__EffectUtil__25:playEffects({ v23 }, nil, v24);
 	end);
 end;
 function v5.playGlitchedLuckyBlockCloneEffect(p16, p17, p18)
 	task.spawn(function()
 		if p18 then
-			local v27 = l__ReplicatedStorage__21.Assets.Effects.GlitchedLuckyBlockTeleportStart:Clone();
+			local v27 = l__ReplicatedStorage__22.Assets.Effects.GlitchedLuckyBlockTeleportStart:Clone();
 		else
-			v27 = l__ReplicatedStorage__21.Assets.Effects.GlitchedLuckyBlockTeleportEnd:Clone();
+			v27 = l__ReplicatedStorage__22.Assets.Effects.GlitchedLuckyBlockTeleportEnd:Clone();
 		end;
 		v27.Name = "GlitchedLuckyBlockEffect";
-		v27.Parent = l__Workspace__22;
+		v27.Parent = l__Workspace__23;
 		v27.Transparency = 1;
-		v27.CFrame = CFrame.new((l__BlockEngine__23:getWorldPosition(p17)));
+		v27.CFrame = CFrame.new((l__BlockEngine__24:getWorldPosition(p17)));
 		local v28 = {};
 		if p18 then
 			local v29 = 2;
@@ -234,7 +237,7 @@ function v5.playGlitchedLuckyBlockCloneEffect(p16, p17, p18)
 			v29 = 1.5;
 		end;
 		v28.destroyAfterSec = v29;
-		l__EffectUtil__24:playEffects({ v27 }, nil, v28);
+		l__EffectUtil__25:playEffects({ v27 }, nil, v28);
 	end);
 end;
 function v5.loopGlitchParticlesOnBlock(p19, p20)
